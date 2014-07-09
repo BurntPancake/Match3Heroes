874263f26f8006bd22809f9b54425050c9fd2225
zd <dongzihua0718@gmail.com>
2014-07-10 01:10:33 +0800

    Board Generation and Mouse hovering effect. Gem selecting effect is not functioning correctly.

---
+++ Macth3Heroes/Assets/Scripts/Cube.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour 
+{
+	public float addScale = 0.2f;
+
+	private int x;
+	private int y; //Position of cube in gem[,] of GenerateBoard
+
+	private Vector3 myScale = new Vector3(1f,1f,1f);
+
+	private bool selected = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void setX(int x)
+	{
+		this.x = x;
+	}
+
+	void setY(int y)
+	{
+		this.y = y;
+	}
+
+	void OnMouseOver()
+	{
+		if(!selected)
+		{		
+			//Hovering block effect
+			iTween.ScaleTo(gameObject, new Vector3(myScale.x+addScale, myScale.y+addScale), 0.1f);
+		}
+
+	}
+	
+	
+	void OnMouseExit()
+	{
+		if(!selected)
+		{		
+			//Exit hovering effect
+		iTween.ScaleTo(gameObject, new Vector3(myScale.x, myScale.y), 0.1f);
+		}
+	}
+}
